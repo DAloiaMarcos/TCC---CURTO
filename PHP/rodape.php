bc5e9d75153de85b5efddd1f3fa7ec0465de0d0c
Aluno <aluno@devisate.local>
2024-04-25 08:11:19 -0300

    alteração footer

--- PHP/rodape.php
+++ PHP/rodape.php
@@ -4,14 +4,34 @@
         <span class="logo-inteira">
             <img src="../CSS/Assets/CURTO.png" alt="Logo-inteira">
         </span>
-        <span class="contatos">
+        <span class="contatos-footer-cima">
             <p>Contatos:</p>
             <p>luan.romao@etec.sp.gov.br</p>
             <p>gabriel.roncon@etec.sp.gov.br</p>
             <p>marcos.daloia@etec.sp.gov.br</p>
         </span>
     </div>
-
+    <div class="container text-white text-center text-lg-start" style="background-color: black; border-radius:20px 20px 0px 0px; display:flex;">
+        <span class="texto-curto">
+           <p>
+            A plataforma que te ajuda a seguir o caminho mais curto na sua carreira
+           </p>
+        </span>
+        <span class="contatos-footer-meio">
+            <p>14 9****-****</p>
+            <a href="#">Instagram</a>
+        </span>
+    </div>
+    <div class="container text-white text-center text-lg-start" style="background-color: black; border-radius:20px 20px 0px 0px; display:flex;">
+        <span class="ano-footer">
+           <h1 class="vq-comercial">2024 &copy;</h1>
+        </span>
+        <span class="links-footer">
+            <a href="#">Feedbacks</a>
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Política de Cookies</a>
+        </span>
+    </div>
 </footer>
 
 
